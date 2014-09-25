58633b219b75257720c7149b1b2f7d1a3f42c78a
gilsondev <contato@gilsondev.com>
2014-09-25 13:33:18 -0300

    Create controller and iterate phones

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,14 @@
 'use strict';
 
+var phonecatApp = angular.module('phonecatApp', []);
+
 /* Controllers */
+
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+    $scope.name = "World";
+    $scope.phones = [
+        {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S'},
+        {'name': 'Motorola XOOM with Wi-fi', 'snippet': 'The Next, Next Generation Tablet'},
+        {'name': 'Motorola XOOM', 'snippet': 'The Next, Next Generation Tablet'},
+    ]
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,10 +2,14 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
+describe('PhoneListCtrl', function() {
+    beforeEach(module('phonecatApp'));
 
-  it("should do something", function() {
+    it("should create 'phones' model with 3 phones", inject(function($controller) {
+        var scope = {};
+        var ctrl = $controller('PhoneListCtrl', {$scope:scope});
 
-  });
-
+        expect(scope.phones.length).toBe(3);
+        excect(scope.name).toBe('World');
+    }));
 });
